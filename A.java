996058634c67c26c456b07d166454263c62f9432
ash-dev <ashishs468@gmail.com>
2025-01-12 16:57:04 +0530

    hdvhhhf

--- A.java
+++ A.java
@@ -1,6 +1,10 @@
 class A{
+    public static int test(){
+        return 7;
+
+    }
     public static void main(String[] args) {
-        System.out.println("ashish");
-        System.out.println("saurav");
+        System.out.println(test());
+       
     
 }}
